6b7ff939f5e9d585b68cc1e01e568fdcc1963f12
Daniel <Danieltwarg@outlook.com>
2025-02-11 22:19:46 -0500

    im done with this

--- src/pages/aboutMe.jsx
+++ src/pages/aboutMe.jsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
     <div class='about'>
       <h1><strong>About Me</strong></h1>
       <div class='about-content'> 
-      <img class='Daniel' src='/Resume picture.jpg' alt='Daniel Wargo' />
+      <img class='Daniel' src='/assets/Resume picture.jpg' alt='Daniel Wargo' />
       <p class='me'>
         Hello, I am Daniel T Wargo, a Full Stack Web Developer with a passion for creating dynamic and user-friendly web applications. I have a strong foundation in HTML, CSS, JavaScript, and React, and I am always eager to learn new technologies and improve my skills. I am currently seeking opportunities to apply my knowledge and contribute to exciting projects. Welcome to my portfolio!
       </p>
