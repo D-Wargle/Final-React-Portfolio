618d6286e6dc905532e47a9c1ec70ee8f5ccabbd
Daniel <Danieltwarg@outlook.com>
2025-02-11 22:10:38 -0500

    aslkjfdsjlf

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -4,12 +4,12 @@ import Project from "../components/project.jsx";
 import "../styles/portfolio.css";
 
 const projects = [
-    { title: "Social Network", image: "public/assets/SOCIAL-NETWORK-API.png", url:"https://github.com/D-Wargle/Social-Network-API-Fix" },
-    { title: "Employee Tracker", image: "public/assets/SQL EMPLOYEE TRACKER.png", url:"https://github.com/D-Wargle/SQL-EmployeeTracker" },
-    { title: "Chartz Music App", image: "public/assets/CHARTZ-Music-app.png", url:"https://github.com/MichaelaHunt/Chartz" },
-    { title: "Kanban Board", image: "public/assets/KANBAN CHALLENGE.png", url:"https://github.com/D-Wargle/Kanban-Challenge" },
-    { title: "Book Search Engine", image: "public/assets/HARRY PUTTER BOOK SEARCH.png", url:"https://github.com/D-Wargle/Book-Search-Engine" },
-    { title: "Weather API", image: "public/assets/WEATHER API.png", url:"https://github.com/D-Wargle/Weather-API-Challenge" },
+    { title: "Social Network", image: "/assets/SOCIAL-NETWORK-API.png", url:"https://github.com/D-Wargle/Social-Network-API-Fix" },
+    { title: "Employee Tracker", image: "/assets/SQL EMPLOYEE TRACKER.png", url:"https://github.com/D-Wargle/SQL-EmployeeTracker" },
+    { title: "Chartz Music App", image: "/assets/CHARTZ-Music-app.png", url:"https://github.com/MichaelaHunt/Chartz" },
+    { title: "Kanban Board", image: "/assets/KANBAN CHALLENGE.png", url:"https://github.com/D-Wargle/Kanban-Challenge" },
+    { title: "Book Search Engine", image: "/assets/HARRY PUTTER BOOK SEARCH.png", url:"https://github.com/D-Wargle/Book-Search-Engine" },
+    { title: "Weather API", image: "/assets/WEATHER API.png", url:"https://github.com/D-Wargle/Weather-API-Challenge" },
 ];
 
 export default function Portfolio() {
