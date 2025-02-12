569ec9e387c1d21b624641f67a4db4c2e8a6e47f
Daniel <Danieltwarg@outlook.com>
2025-02-11 22:18:02 -0500

    aslkdfj;alsdk

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -4,7 +4,7 @@ import Project from "../components/project.jsx";
 import "../styles/portfolio.css";
 
 const projects = [
-    { title: "Social Network", image: "/SOCIAL-NETWORK-API.png", url:"https://github.com/D-Wargle/Social-Network-API-Fix" },
+    { title: "Social Network", image: "/assets/SOCIAL-NETWORK-API.png", url:"https://github.com/D-Wargle/Social-Network-API-Fix" },
     { title: "Employee Tracker", image: "/SQL-EMPLOYEE-TRACKER.png", url:"https://github.com/D-Wargle/SQL-EmployeeTracker" },
     { title: "Chartz Music App", image: "/CHARTZ-Music-app.png", url:"https://github.com/MichaelaHunt/Chartz" },
     { title: "Kanban Board", image: "/KANBAN CHALLENGE.png", url:"https://github.com/D-Wargle/Kanban-Challenge" },
